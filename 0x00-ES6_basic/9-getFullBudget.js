c40cbae2848d72caddb2e042f2fb35cd57b7ba7e
chan.gretta <93374467+william-4@users.noreply.github.com>
2023-02-13 16:31:48 +0300

    Update 9-getFullBudget.js

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -3,9 +3,9 @@ import getBudgetObject from './7-getBudgetObject';
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
-      ...budget,
-      getIncomeInDollars: (income) => `$${income}`,
-      getIncomeInEuros: (income) => `${income} euros`,
+    ...budget,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
   };
 
   return fullBudget;
